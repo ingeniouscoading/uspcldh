44c88cae629d404f4c1eaf193c6f6c6d0874d75d
Ingenious Abhinav <125333790+ingeniouscoading@users.noreply.github.com>
2023-02-17 11:24:10 +0530

    Apps

--- uspccore/settings.py
+++ uspccore/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'uspcldh', # USPC Website
+    'uspcldh.apps.UspcldhConfig', # USPC Website
 ]
 
 MIDDLEWARE = [
